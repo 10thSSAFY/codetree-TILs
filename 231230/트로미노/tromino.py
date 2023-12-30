625ab054be2435c1ff8d93ccc8f4a822c2e8fbc2
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2023-12-30 20:34:58 +0900

    Title: 트로미노, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 231230/트로미노/tromino.py
+++ 231230/트로미노/tromino.py
@@ -1,37 +1,20 @@
-def check_L(R, C):
-    global res
-    tmp = arr[R][C]
-    area = 0
-    if R <= n-2 and C <= m-2:
-        for dR in range(2):
-            for dC in range(2):
-                tmp = min(tmp, arr[R+dR][C+dC])
-                area += arr[R+dR][C+dC]
-    area -= tmp
-    res = max(res, area)
-
-
-def check_I(R, C):
-    global res
-    if R <= n-3:
-        tmp = 0
-        for i in range(3):
-            tmp += arr[R+i][C]
-        res = max(res, tmp)
-    if C <= m-3:
-        tmp = 0
-        for i in range(3):
-            tmp += arr[R][C+i]
-        res = max(res, tmp)
-
-
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 res = 0
 for r in range(n):
     for c in range(m):
-        check_L(r, c)
-        check_I(r, c)
+        for delta in [((-1, 0), (0, -1)), ((-1, 0), (0, 1)),
+                      ((1, 0), (0, -1)), ((1, 0), (0, 1)),
+                      ((0, -1), (0, 1)), ((-1, 0), (1, 0))]:
+            tmp = arr[r][c]
+            for dr, dc in delta:
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < n and 0 <= nc < m:
+                    tmp += arr[nr][nc]
+                else:
+                    break
+            else:
+                res = max(res, tmp)
 
 print(res)
