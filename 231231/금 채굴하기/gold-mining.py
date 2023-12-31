c37b5e1a8b9b4c04463aad23928d86254ee5c36c
10thSSAFY <139323308+10thSSAFY@users.noreply.github.com>
2023-12-31 19:17:39 +0900

    Title: 금 채굴하기, Time: 270ms, Memory: 26MB, Status: Passed - CodeTree

--- 231231/금 채굴하기/gold-mining.py
+++ 231231/금 채굴하기/gold-mining.py
@@ -1,29 +1,16 @@
-def do(i, j):
-    global res
-    gold = money = 0
-
-    for k in range(n+1):
-        for r in range(-k, k+1):
-            if i+r < 0 or n <= i+r:
-                continue
-            for c in range(-k, k+1):
-                if j+c < 0 or n <= j+c:
-                    continue
-                if abs(r) + abs(c) == k:
-                    if arr[i+r][j+c] == 1:
-                        gold += 1
-                        money += m
-
-        if money >= k*k + (k+1)*(k+1):
-            res = max(res, gold)
-
-
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 res = 0
-for i in range(n):
-    for j in range(n):
-        do(i, j)
+for K in range(n+1):
+    for r in range(n):
+        for c in range(n):
+            gold = 0
+            for dr in range(n):
+                for dc in range(n):
+                    if abs(dr - r) + abs(dc - c) <= K:
+                        gold += arr[dr][dc]
+            if gold * m >= K * K + (K + 1) * (K + 1):
+                res = max(res, gold)
 
 print(res)
